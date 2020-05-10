0ce597e9171ac0b93a4117d7db306f7861baec31
gpenki <gpenki@gmail.com>
2020-05-10 04:07:29 -0500

    Delete UserSequencesRepository.java

--- UserSequencesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mt.springbootmongo.repository;
-
-import com.mt.springbootmongo.domain.UserSequences;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserSequencesRepository extends MongoRepository<UserSequences, String> {
-}
